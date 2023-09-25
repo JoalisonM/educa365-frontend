eef424d402b8499309d897ffea0d857c2f95488a
Joalison Matheus <joalison.matheus@moorse.io>
2023-09-24 23:07:17 -0300

    responsive table

--- src/pages/Employee/AddressFieldsForm.tsx
+++ src/pages/Employee/AddressFieldsForm.tsx
@@ -66,7 +66,7 @@ export const AddressFieldsForm = ({ form, errors }: AddressFieldsFormProps) => {
           )}
         />
       </div>
-      <div className="grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-8">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-8">
         <FormField
           control={form.control}
           name="endereco.numero"
--- src/pages/Employee/index.tsx
+++ src/pages/Employee/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
+import * as ScrollArea from "@radix-ui/react-scroll-area";
 import {
   MagnifyingGlass,
   PencilSimpleLine,
@@ -41,7 +42,7 @@ export const Employee = () => {
 
   return (
     <div className="flex flex-col gap-6">
-      <div className="flex items-center justify-between pb-5 border-b border-gray-200">
+      <div className="flex flex-col gap-4 pb-5 border-b border-gray-200 md:flex-row md:items-center md:justify-between">
         <h1 className="text-3xl font-bold text-zinc-800">Funcionários</h1>
 
         <Dialog.Root>
@@ -61,66 +62,77 @@ export const Employee = () => {
         </Dialog.Root>
       </div>
 
-      <Input.Root className="mx-1 flex w-96 items-center gap-2 rounded-full bg-gray-50 border border-gray-300 px-3 py-2 shadow-sm">
+      <Input.Root className="mx-1 flex items-center gap-2 rounded-full bg-gray-50 border border-gray-300 px-3 py-2 shadow-sm md:w-96">
         <Input.Prefix>
           <MagnifyingGlass className="h-5 w-5 text-zinc-500" />
         </Input.Prefix>
         <Input.Control placeholder="Pesquisar" />
       </Input.Root>
 
-      <table className="mt-4 table-auto border-separate border-spacing-y-2">
-        <thead>
-          <tr className="text-left text-grayTableTitle">
-            <th className="py-4 px-8">Nome</th>
-            <th className="py-4 px-8">E-mail</th>
-            <th className="py-4 px-8">Cargo</th>
-            <th className="py-4 px-8">Nascimento</th>
-          </tr>
-        </thead>
-        <tbody>
-          {employees &&
-            employees.map((employee) => (
-              <tr
-                key={employee.id}
-                className="bg-zinc-100 text-zinc-700 text-left"
-              >
-                <td className="rounded-tl-md rounded-bl-md py-5 px-8">
-                  {employee.nome}
-                </td>
-                <td className="py-5 px-8">{employee.email}</td>
-                <td className="py-5 px-8">{employee.cargo}</td>
-                <td className="py-5 px-8">{employee.dataNascimento}</td>
-                <td className=" rounded-tr-md rounded-br-md py-5 px-8 text-right space-x-4">
-                  <Dialog.Root>
-                    <Dialog.Trigger asChild>
-                      <Button
-                        type="button"
-                        variant="ghost"
-                        onClick={() => handleShowEmployee(employee.id)}
-                      >
-                        <PencilSimpleLine className="h-5 w-5" />
-                      </Button>
-                    </Dialog.Trigger>
-
-                    <Drawer title="Registro de funcionário">
-                      <EmployeeForm />
-                    </Drawer>
-                  </Dialog.Root>
-                  <AlertDialog
-                    id={employee.id}
-                    title="Você tem certeza absoluta?"
-                    description="Essa ação não pode ser desfeita. Isso excluirá permanentemente o funcionário."
-                    onDelete={handleDelete}
-                  >
-                    <Button type="button" variant="danger">
-                      <Trash className="h-5 w-5" />
-                    </Button>
-                  </AlertDialog>
-                </td>
+      <ScrollArea.Root className="w-full" type="scroll">
+        <ScrollArea.Viewport className="w-full overflow-x-scroll">
+          <table className="mt-4 w-full table-auto border-separate border-spacing-y-2">
+            <thead>
+              <tr className="text-left text-grayTableTitle">
+                <th className="py-4 px-8 whitespace-nowrap">Nome</th>
+                <th className="py-4 px-8 whitespace-nowrap">E-mail</th>
+                <th className="py-4 px-8 whitespace-nowrap">Cargo</th>
+                <th className="py-4 px-8 whitespace-nowrap">Nascimento</th>
               </tr>
-            ))}
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+              {employees &&
+                employees.map((employee) => (
+                  <tr
+                    key={employee.id}
+                    className="bg-zinc-100 text-zinc-700 text-left"
+                  >
+                    <td className="rounded-tl-md rounded-bl-md py-5 px-8">
+                      {employee.nome}
+                    </td>
+                    <td className="py-5 px-8">{employee.email}</td>
+                    <td className="py-5 px-8">{employee.cargo}</td>
+                    <td className="py-5 px-8">{employee.dataNascimento}</td>
+                    <td className=" rounded-tr-md rounded-br-md py-5 px-8 text-right space-x-4">
+                      <Dialog.Root>
+                        <Dialog.Trigger asChild>
+                          <Button
+                            type="button"
+                            variant="ghost"
+                            onClick={() => handleShowEmployee(employee.id)}
+                          >
+                            <PencilSimpleLine className="h-5 w-5" />
+                          </Button>
+                        </Dialog.Trigger>
+
+                        <Drawer title="Registro de funcionário">
+                          <EmployeeForm />
+                        </Drawer>
+                      </Dialog.Root>
+                      <AlertDialog
+                        id={employee.id}
+                        title="Você tem certeza absoluta?"
+                        description="Essa ação não pode ser desfeita. Isso excluirá permanentemente o funcionário."
+                        onDelete={handleDelete}
+                      >
+                        <Button type="button" variant="danger">
+                          <Trash className="h-5 w-5" />
+                        </Button>
+                      </AlertDialog>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </ScrollArea.Viewport>
+
+        <ScrollArea.Scrollbar
+          className="flex h-1 touch-none select-none bg-zinc-100"
+          orientation="horizontal"
+        >
+          <ScrollArea.Thumb className="relative flex-1 rounded-lg bg-zinc-300" />
+        </ScrollArea.Scrollbar>
+      </ScrollArea.Root>
     </div>
   );
 };
